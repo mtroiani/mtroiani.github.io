2163231f868bb2131b3df4517178ebe8440faeb6
mtroiani <marygresh@Mary-Greshs-MacBook-Pro.local>
2016-06-26 08:03:14 -0700

    Add page content
    Add content files for github portfolio.

---
+++ Javascript/mtroianiPortfolio.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        sectionsColor: ['#f2f2f2', '#54445C', '#444C5C', '#445C54', '#48445C', '#92AAC7'],
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+        menu: '#menu',
+        paddingTop: '5%',
+        slidesNavigation: 'true'
+    });
+});
