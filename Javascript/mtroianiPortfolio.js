19a936c7fc3cba7847f3eda63969b3e791037d23
mtroiani <marygresh@Mary-Greshs-MacBook-Pro.local>
2016-08-12 18:05:02 -0700

    Add content
    Adds two new projects, updates url anchors (descriptive instead of
    #page1 #page2 etc), and minor text changes.

--- Javascript/mtroianiPortfolio.js
+++ Javascript/mtroianiPortfolio.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         sectionsColor: ['#f2f2f2', '#54445C', '#444C5C', '#445C54', '#48445C', '#92AAC7'],
-        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+        anchors: ['homepage', 'aboutme', 'myskills', 'mywebprojects', 'mydataprojects', 'contactme'],
         menu: '#menu',
         paddingTop: '5%',
         slidesNavigation: 'true'
