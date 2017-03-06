623f1307f27f4d14d111f19e7aaa4834e9f9ec55
Mary Troiani <mary.gresh@gmail.com>
2017-03-05 17:13:09 -0800

    feat: update background colors

--- Javascript/mtroianiPortfolio.js
+++ Javascript/mtroianiPortfolio.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        sectionsColor: ['#f2f2f2', '#54445C', '#444C5C', '#445C54', '#48445C', '#92AAC7'],
+        sectionsColor: ['#f2f2f2', '#48445C', '#48445C', '#48445C', '#48445C', '#92AAC7'],
         anchors: ['homepage', 'aboutme', 'myskills', 'mywebprojects', 'mydataprojects', 'contactme'],
         menu: '#menu',
         paddingTop: '5%',
